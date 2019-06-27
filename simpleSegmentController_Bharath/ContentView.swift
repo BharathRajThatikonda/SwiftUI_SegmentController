b62d6089acdb5431285b593539721921264d19fd
“Bharath <thatikondabharathraj@gmail.com>
2019-06-28 02:06:16 +0530

    Initial Commit

---
+++ simpleSegmentController_Bharath/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  simpleSegmentController_Bharath
+//
+//  Created by Bharath Thatikonda on 6/28/19.
+//  Copyright © 2019 Bharath Thatikonda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ simpleSegmentController_BharathTests/simpleSegmentController_BharathTests.swift
@@ -0,0 +1,34 @@
+//
+//  simpleSegmentController_BharathTests.swift
+//  simpleSegmentController_BharathTests
+//
+//  Created by Bharath Thatikonda on 6/28/19.
+//  Copyright © 2019 Bharath Thatikonda. All rights reserved.
+//
+
+import XCTest
+@testable import simpleSegmentController_Bharath
+
+class simpleSegmentController_BharathTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
